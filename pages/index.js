b2fe110e63468567cbceddfe8e5523c0d6baa6df
tgarg912 <tgarg@villanova.edu>
2024-12-11 12:50:02 -0500

    final commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [isModal, setIsModal] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
